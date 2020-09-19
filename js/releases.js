15a26d835e5366145d85f32d44cf066a0ea3fc22
Marquis Kurt <software@marquiskurt.net>
2020-09-19 18:05:48 -0400

    [betaseed] Add releases page
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- js/releases.js
+++ js/releases.js
@@ -10,17 +10,69 @@ function makeReleaseHtml(version, notes) {
   return `<h2>Updates for ${version}</h2><p><ul>${notesLog}</ul></p>`;
 }
 
-function getReleaseNotes() {
-  return fetch("../devchanges.json").then((resp) => resp.json());
+function getDeveloperReleaseNotes() {
+  return fetch("./developer.json").then((resp) => resp.json());
+}
+
+function getStableReleaseNotes() {
+  return fetch("./release.json").then((resp) => resp.json());
 }
 
 function updateReleases() {
-  getReleaseNotes()
+  getStableReleaseNotes()
     .then(resp => {
-      let releases = document.getElementById("releases");
+      let releases = document.getElementById("releases-stable");
+      let newHTML = "";
       for (const release in resp) {
         const releaseID = release.replaceAll(".", "_")
-        releases.innerHTML += `<div id="release_${releaseID}">${makeReleaseHtml(release, resp[release])}</div>`;
+        newHTML += `<div id="release_${releaseID}">${makeReleaseHtml(release, resp[release])}</div>`;
       }
+      releases.innerHTML = newHTML ?? "";
     })
+    .catch(err => {
+      console.error(err);
+      releases.innerHTML = `<p style="text-align: center">Failed to get stable release information.</p>`;
+    })
+  getDeveloperReleaseNotes()
+    .then(resp => {
+      let releases = document.getElementById("releases-developer");
+      let newHTML = "";
+      for (const release in resp) {
+        const releaseID = release.replaceAll(".", "_")
+        newHTML += `<div id="release_${releaseID}">${makeReleaseHtml(release, resp[release])}</div>`;
+      }
+      releases.innerHTML = newHTML ?? "";
+    })
+    .catch(err => {
+      console.error(err);
+      releases.innerHTML = `<p style="text-align: center">Failed to get developer release information.</p>`;
+    })
+}
+
+function showStableReleaseNotes() {
+  let stable = document.getElementById("releases-stable");
+  let developer = document.getElementById("releases-developer");
+
+  let btnBeta = document.getElementById("btn-beta");
+  let btnStable = document.getElementById("btn-stable");
+
+  stable.classList.remove("releases-hide");
+  developer.classList.add("releases-hide");
+
+  btnBeta.classList.remove("active");
+  btnStable.classList.add("active");
+}
+
+function showDeveloperReleaseNotes() {
+  let stable = document.getElementById("releases-stable");
+  let developer = document.getElementById("releases-developer");
+
+  let btnBeta = document.getElementById("btn-beta");
+  let btnStable = document.getElementById("btn-stable");
+
+  stable.classList.add("releases-hide");
+  developer.classList.remove("releases-hide");
+
+  btnStable.classList.remove("active");
+  btnBeta.classList.add("active");
 }
